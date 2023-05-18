fabca73585eb6bba0d092fe0b9d0929857f6aa2c
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-05-18 21:46:32 +0200

    Created insert db ops with command design pattern

---
+++ progtech_beadando/src/main/java/DatabaseConnection/InsertBingoFieldCommand.java
@@ -0,0 +1,42 @@
+package DatabaseConnection;
+
+import org.apache.log4j.Logger;
+
+public class InsertBingoFieldCommand implements Command {
+
+    private Logger logger = Logger.getLogger("Insert bingo field logger");
+    private String fieldText;
+    private String username;
+    private DatabaseConnection databaseConnection;
+
+    public InsertBingoFieldCommand(String fieldText, DatabaseConnection databaseConnection) {
+        this.fieldText = fieldText;
+        this.databaseConnection = databaseConnection;
+    }
+
+    public InsertBingoFieldCommand(String fieldText, String username, DatabaseConnection databaseConnection) {
+        this(fieldText, databaseConnection);
+        this.username = username;
+    }
+
+    @Override
+    public void execute() {
+
+        try {
+
+            if (this.username == null) {
+                this.databaseConnection.getDbConnection().createStatement().executeUpdate("insert into bingo_fields (field_text) values ('"+ this.fieldText +"')");
+                logger.info(this.fieldText + " inserted into database!");
+            }
+            else {
+                this.databaseConnection.getDbConnection().createStatement().executeUpdate("insert into bingo_fields (field_text, uploaded_by) values ('"+ this.fieldText +"', '"+ this.username +"')");
+                logger.info(this.fieldText + " inserted into database by: "+ this.username +"!");
+            }
+
+        }
+        catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+
+    }
+}
---
+++ progtech_beadando/src/main/java/DatabaseConnection/InsertScoreCommand.java
@@ -0,0 +1,28 @@
+package DatabaseConnection;
+
+import org.apache.log4j.Logger;
+
+public class InsertScoreCommand implements Command {
+
+    private Logger logger = Logger.getLogger("Insert score logger");
+    private String username;
+    private DatabaseConnection databaseConnection;
+
+    public InsertScoreCommand(String username, DatabaseConnection databaseConnection) {
+        this.username = username;
+        this.databaseConnection = databaseConnection;
+    }
+
+    @Override
+    public void execute() {
+
+        try {
+            this.databaseConnection.getDbConnection().createStatement().executeUpdate("insert into score (username) values ('"+this.username+"')");
+            logger.info(this.username + " inserted into database!");
+        }
+        catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+
+    }
+}
