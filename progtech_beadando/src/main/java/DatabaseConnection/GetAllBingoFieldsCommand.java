fa50ba2a45f9c3005a126040b833d702a1d7c189
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-05-19 16:25:18 +0200

    Some minor refactoring - GetAllBingoFields
    - Deleted unnecessary try-catch block
    + Added arraylist to store query results in it
    + Changed logger message types
    + Added error and warning messages in dialogboxes

--- progtech_beadando/src/main/java/DatabaseConnection/GetAllBingoFieldsCommand.java
+++ progtech_beadando/src/main/java/DatabaseConnection/GetAllBingoFieldsCommand.java
@@ -2,17 +2,30 @@ package DatabaseConnection;
 
 import org.apache.log4j.Logger;
 
+import javax.swing.*;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class GetAllBingoFieldsCommand implements Command{
 
     private Logger logger = Logger.getLogger("Get all bingo fields logger");
+
+    private JFrame frame = new JFrame();
+
     private DatabaseConnection databaseConnection;
 
     private ResultSet results;
 
+    private ArrayList<String> listOfBingoFieldTexts;
+
     public GetAllBingoFieldsCommand(DatabaseConnection databaseConnection) {
         this.databaseConnection = databaseConnection;
+        listOfBingoFieldTexts = new ArrayList<String>();
+    }
+
+
+    public ArrayList<String> getListOfBingoFieldTexts() {
+        return listOfBingoFieldTexts;
     }
 
     @Override
@@ -21,19 +34,16 @@ public class GetAllBingoFieldsCommand implements Command{
 
             this.results = this.databaseConnection.getDbConnection().createStatement().executeQuery("select * from bingo_fields");
 
-            try
-            {
-                while (this.results.next()) {
-                    System.out.println(this.results.getString("field_text"));
-                }
-                logger.info("Query succeeded!");
+            while (this.results.next()) {
+                listOfBingoFieldTexts.add(this.results.getString("field_text"));
             }
-            catch (Exception e) {
-                logger.warn(e.getMessage());
-            }
+
+            logger.info("Query succeeded!");
         }
         catch (Exception e) {
-            logger.warn(e.getMessage());
+            logger.error(e.getMessage());
+
+            JOptionPane.showMessageDialog(frame, e.getMessage(), "Get all bingo fields error", JOptionPane.ERROR_MESSAGE);
         }
 
     }
